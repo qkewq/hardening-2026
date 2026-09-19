8711563006f573a8e9411e1e67ffe540ee620d4d
kwood <155027796+qkewq@users.noreply.github.com>
2026-09-19 08:57:07 -0700

    Create linux-fw.sh

---
+++ linux-fw.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+iptables -L
+
+iptables -L > /tmp/firewall_$(date +%R)
+
+read -rp "TCP Inbound Ports: " PORTS
+
+IFS=','
+for PORT in $PORTS;do
+        iptables -A INPUT -p tcp --dport "$PORT" -j ACCEPT
+done
+
+read -rp "UDP Inbound Ports: " PORTS
+
+IFS=','
+for PORT in $PORTS;do
+        iptables -A INPUT -p udp --dport "$PORT" -j ACCEPT
+done
+
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A OUTPUT -o lo -j ACCEPT
+
+iptables --policy INPUT DROP
+iptables --policy OUTPUT DROP
+iptables --policy FORWARD DROP
+
+iptables -A OUTPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
